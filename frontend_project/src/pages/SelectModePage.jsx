70459a4596ac4b70a1cf1db35ca3119488b370c6
sibainuyou <otsuka@m.cs.osakafu-u.ac.jp>
2023-07-11 16:16:15 +0900

    changed SelectModePage.jsx

--- frontend_project/src/pages/SelectModePage.jsx
+++ frontend_project/src/pages/SelectModePage.jsx
@@ -1,9 +1,33 @@
-
+import { Navigate } from 'react-router-dom';
 
 const SelectModePage = () => {
+  const handleCreateMonster = () => {
+    // モンスターを作成ボタンがクリックされた時の処理
+    return <Navigate to="/selectMode" replace />;
+  };
+
+  const handleTalkToMonster = () => {
+    // モンスターと会話ボタンがクリックされた時の処理
+    return <Navigate to="/selectMonster" replace />;
+  };
+
   return (
-    <div>SelectModePage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl mb-8">モンスターゲーム</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+        onClick={handleCreateMonster}
+      >
+        モンスターを作成
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleTalkToMonster}
+      >
+        モンスターと会話
+      </button>
+    </div>
+  );
+};
 
 export default SelectModePage
